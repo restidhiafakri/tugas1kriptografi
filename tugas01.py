8ebfc5b5d8f62103f9c45585a9a3825aec7c0fa8
restidhiafakri <58275136+restidhiafakri@users.noreply.github.com>
2022-01-03 22:33:08 +0800

    Add files via upload

---
+++ tugas01.py
@@ -0,0 +1,36 @@
+print ("Tugas 01 Kriptografi")
+print ("Nama : Resti")
+print ("Nim  : E1E1 19 012")
+
+def encript(plaintext, key):
+    encoded = []
+
+    for i in range(len(plaintext)):
+        xor = ord(plaintext[i]) ^ ord(key[i % len(key)])
+        encoded.append(chr(xor))
+
+    return ''.join(encoded)
+
+def decript(chipertext, key):
+    return encript(chipertext, key)
+
+def main():
+    print('Pilih Menu: \n1. Enkripsi\n2. Dekripsi')
+    pilihan = int(input('Pilihan: '))
+
+    if pilihan == 1:
+        plaintext = str(input('Masukkan plaintext: '))
+        key = str(input('Masukkan key: '))
+
+        print('Plaintext: ', plaintext)
+        print('Enkripsi: ', encript(plaintext, key))
+    elif pilihan == 2:
+        chipertext = str(input('Masukkan chipertext: '))
+        key = str(input('Masukkan key: '))
+
+        print('Chipertext: ', chipertext)
+        print('Dekripsi: ', decript(chipertext, key))
+    else:
+        exit()
+
+main()
